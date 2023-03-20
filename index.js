9d273e46d8218c65ec53a5922cf5f1afebc6e1b0
Mohammad <moh.hossain98@gmail.com>
2023-03-20 14:55:12 -0400

    starter code

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 console.log("Hello World");
 
+// What is a function?
+
+// A function is a block of code that performs a specific task. A function is reusable and can be called multiple times. It minimizes the amount of code that you have to write. It is a way to organize your code.
+
+// Function declaration
+
+// Real world Examples
+
+// 1. You want make an orange juice. You need to get the oranges, cut them, squeeze them, and then drink it. You need to repeat the same steps every time you want to make an orange juice. However, if you have a juicer machine, you can just put the oranges in the machine and it will do all the work for you. You can just drink the juice. The juicer machine is a function. It performs a specific task. You can juice multiple fruits multiple times.
+
+// Callback functions
+
+// A callback function is a function that is passed as an argument to another function. It is executed after the parent function has finished executing.
+
+// Why do we use a callback function?
+
+// 1. To make sure that a function is executed after another function has finished executing.
+// Example:
+
+// function firstFunction() {
+//   console.log("First function is executed");
+// }
+
+// function secondFunction(callback) {
+//   console.log("Second function is executed");
+//   callback();
+// }
+
+// secondFunction(firstFunction);
+
+// Arrow functions
+// Arrow functions are a shorter way to write functions. They are also called fat arrow functions. Arrow functions can be anonymous or named. They are always anonymous if they are assigned to a variable. Arrow functions can be as an anonymous callback function. You can hois
