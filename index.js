83726b028f6ceaa4cf73e0a12c6c58635692e657
Mohammad <moh.hossain98@gmail.com>
2023-03-22 15:11:05 -0400

    starter code

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-console.log("This is the JS file");
+const cats = [
+  {
+    name: "Fluffy",
+    age: 2,
+    breed: "Persian",
+    personality: "Affectionate",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/White_Persian_Cat.jpg/220px-White_Persian_Cat.jpg",
+  },
+  {
+    name: "Socks",
+    age: 4,
+    breed: "Siamese",
+    personality: "Playful",
+    image:
+      "https://www.purina.co.uk/sites/default/files/styles/square_medium_440x440/public/2022-06/Siamese%201.jpg?h=c17eaee4&itok=BRsemy6v",
+  },
+  {
+    name: "Whiskers",
+    age: 1,
+    breed: "Maine Coon",
+    personality: "Curious",
+    image:
+      "https://westparkanimalhospital.com/wp-content/uploads/2019/05/WestPark_iStock-600994082-1024x706-1.jpg",
+  },
+  {
+    name: "Mittens",
+    age: 5,
+    breed: "Tabby",
+    personality: "Independent",
+    image:
+      "https://www.thesprucepets.com/thmb/APYdMl_MTqwODmH4dDqaY5q0UoE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/all-about-tabby-cats-552489-hero-a23a9118af8c477b914a0a1570d4f787.jpg",
+  },
+];
 
-let foods = ["pizza", "shawarma", "ice-cream"];
-
-document.getElementsByTagName("p")[0].textContent =
-  "The Document Object Model (DOM) is a programming interface for web documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects; that way, programming languages can interact with the page.";
-
-let newP = document.createElement("p");
-newP.textContent = "This is a new P tag";
-
-let ul = document.createElement("ul");
-
-for (let food of foods) {
-  console.log(food);
-  let li = document.createElement("li");
-  li.textContent = food;
-  ul.appendChild(li);
-}
-
-foods.forEach((food) => {
-  let li = document.createElement("li");
-  li.textContent = food;
-  ul.appendChild(li);
-});
-
-// foods.forEach();
-
-let div = document.querySelector("div");
-div.append(newP, ul);
-
-// div.remove();
+console.log(cats);
