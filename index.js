6d72e6bd766ee98898cd63be46e815947ca33db3
Mohammad <moh.hossain98@gmail.com>
2023-03-22 16:45:19 -0400

    lecture code

--- index.js
+++ index.js
@@ -33,4 +33,88 @@ const cats = [
   },
 ];
 
+// we have an array of objects
+// we iterate over the array
+// we iterate over the objects
+// we create a new element for each object
+// we append it to the div with the class cat-list
+
 console.log(cats);
+
+// querySelector
+
+// for (let cat of cats) {
+//   console.log(cat.name, cat["age"]);
+//   const card = document.createElement("div");
+
+//   card.classList.add("cat-card");
+
+//   const image = document.createElement("img");
+//   image.src = cat.image;
+
+//   const name = document.createElement("p");
+//   name.textContent = cat.name;
+
+//   const age = document.createElement("p");
+//   age.textContent = cat.age;
+
+//   const breed = document.createElement("p");
+//   breed.textContent = cat.breed;
+
+//   const personality = document.createElement("p");
+//   personality.textContent = cat.personality;
+
+//   card.append(image, name, age, breed, personality);
+
+//   let cat_list = document.getElementsByClassName("cat-list");
+
+//   console.log(cat_list);
+//   cat_list[0].append(card);
+// }
+
+cats.forEach((cat) => {
+  console.log(cat.name, cat["age"]);
+
+  let adopted = false;
+  const card = document.createElement("div");
+
+  card.classList.add("cat-card");
+
+  const image = document.createElement("img");
+  image.src = cat.image;
+
+  const name = document.createElement("p");
+  name.textContent = cat.name;
+
+  const age = document.createElement("p");
+  age.textContent = cat.age;
+
+  const breed = document.createElement("p");
+  breed.textContent = cat.breed;
+
+  const personality = document.createElement("p");
+  personality.textContent = cat.personality;
+
+  const button = document.createElement("button");
+  button.textContent = "Adopt";
+
+  button.addEventListener("click", () => {
+    console.log("Button is clicked!");
+    adopted = !adopted;
+
+    if (adopted) {
+      button.textContent = "Adopted";
+      card.style.backgroundColor = "grey";
+    } else {
+      button.textContent = "Adopt";
+      card.style.backgroundColor = "white";
+    }
+  });
+
+  card.append(image, name, age, breed, personality, button);
+
+  let cat_list = document.getElementsByClassName("cat-list");
+
+  console.log(cat_list);
+  cat_list[0].append(card);
+});
